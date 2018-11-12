f5f958bb2ecf0e63d51e92d855f5445075e38e96
Dominik Thalhammer <dominik@thalhammer.it>
2018-11-12 11:17:17 +0100

    Fixed qapi_Timer_Sleep, again

--- examples/common/txm_lib/qapi_timer_user.c
+++ examples/common/txm_lib/qapi_timer_user.c
@@ -1,5 +1,9 @@
 #include "qapi_timer.h"
 
+#ifndef SLEEP_WORKAROUND
+#define SLEEP_WORKAROUND 1
+#endif
+
 qapi_Status_t qapi_time_get(qapi_time_unit_type time_get_unit, qapi_time_get_t* time) {
 	return (qapi_Status_t)_txm_module_system_call12(TXM_QAPI_TIME_GET, (ULONG)time_get_unit, (ULONG)time, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 }
@@ -12,28 +16,78 @@ qapi_Status_t qapi_Timer_Get_Timer_Info(qapi_TIMER_handle_t timer_handle, qapi_T
 	return (qapi_Status_t)_txm_module_system_call12(TXM_QAPI_TIMER_GET_INFO, (ULONG)timer_handle, (ULONG)timer_info, (ULONG)data, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 }
 
+#if SLEEP_WORKAROUND
+static void _timer_set_evt(void* evt) {
+	tx_event_flags_set((TX_EVENT_FLAGS_GROUP*)evt, 0x1, TX_OR);
+}
+
+static qapi_Status_t _sleep_timer(uint64_t timeout, qapi_TIMER_unit_type unit) {
+	TX_EVENT_FLAGS_GROUP evt_flags;
+	qapi_TIMER_handle_t timer_handle;
+	qapi_TIMER_define_attr_t timer_def_attr;
+	timer_def_attr.cb_type = QAPI_TIMER_FUNC1_CB_TYPE;
+	timer_def_attr.sigs_func_ptr = (void*)&_timer_set_evt;
+	timer_def_attr.sigs_mask_data = (uint32_t)&evt_flags;
+	timer_def_attr.deferrable = false;
+
+	int res = qapi_Timer_Def(&timer_handle, &timer_def_attr);
+	if(res != 0) return QAPI_ERR_NO_MEMORY;
+
+	res = tx_event_flags_create(&evt_flags, "timer_wait");
+	if(res != 0) {
+		qapi_Timer_Undef(timer_handle);
+		return QAPI_ERR_NO_MEMORY;
+	}
+
+	qapi_TIMER_set_attr_t timer_set_attr; 
+	timer_set_attr.reload = 0;
+	timer_set_attr.time = timeout;
+	timer_set_attr.unit = unit; 
+
+	res = qapi_Timer_Set(timer_handle, &timer_set_attr);
+	if(res != 0) {
+		qapi_Timer_Undef(timer_handle);
+		tx_event_flags_delete(&evt_flags);
+		return QAPI_ERR_NO_MEMORY;
+	}
+
+	ULONG flags;
+	res = tx_event_flags_get(&evt_flags, 0x1, TX_AND_CLEAR, &flags, TX_WAIT_FOREVER);
+	if(res != 0) {
+		qapi_Timer_Stop(timer_handle);
+		qapi_Timer_Undef(timer_handle);
+		tx_event_flags_delete(&evt_flags);
+		return QAPI_ERR_NO_MEMORY;
+	}
+
+	qapi_Timer_Stop(timer_handle);
+	qapi_Timer_Undef(timer_handle);
+	tx_event_flags_delete(&evt_flags);
+	return QAPI_OK;
+}
+
 qapi_Status_t qapi_Timer_Sleep(uint64_t timeout, qapi_TIMER_unit_type unit, qbool_t non_deferrable) {
 	switch(unit) {
 	case QAPI_TIMER_UNIT_TICK:
 		return (qapi_Status_t)_txm_module_system_call12(TXM_QAPI_TIMER_SLEEP, (ULONG)unit, (ULONG)timeout, (ULONG)non_deferrable, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 	case QAPI_TIMER_UNIT_USEC:
-		return (qapi_Status_t)_txm_module_system_call12(TXM_QAPI_TIMER_SLEEP, (ULONG)unit, (ULONG)(timeout*19.2), (ULONG)non_deferrable, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		return QAPI_ERR_NOT_SUPPORTED;
 	case QAPI_TIMER_UNIT_MSEC:
-		return (qapi_Status_t)_txm_module_system_call12(TXM_QAPI_TIMER_SLEEP, (ULONG)QAPI_TIMER_UNIT_TICK, (ULONG)timeout*19200, (ULONG)non_deferrable, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+		if(timeout < 10) return QAPI_ERR_NOT_SUPPORTED;
 	case QAPI_TIMER_UNIT_SEC:
-		for(uint64_t i = 0; i < timeout; i++) {
-			qapi_Status_t res = (qapi_Status_t)_txm_module_system_call12(TXM_QAPI_TIMER_SLEEP, (ULONG)QAPI_TIMER_UNIT_TICK, (ULONG)timeout*19200081, (ULONG)non_deferrable, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-			if(res != QAPI_OK) return res;
-		}
-		return QAPI_OK;
 	case QAPI_TIMER_UNIT_MIN:
-		return qapi_Timer_Sleep(timeout * 60, QAPI_TIMER_UNIT_SEC, non_deferrable);
 	case QAPI_TIMER_UNIT_HOUR:
-		return qapi_Timer_Sleep(timeout * 3600, QAPI_TIMER_UNIT_SEC, non_deferrable);
+		return _sleep_timer(timeout, unit);
 	default:
 		return QAPI_ERR_INVALID_PARAM;
 	}
 }
+
+#else
+qapi_Status_t qapi_Timer_Sleep(uint64_t timeout, qapi_TIMER_unit_type unit, qbool_t non_deferrable) {
+	return (qapi_Status_t)_txm_module_system_call12(TXM_QAPI_TIMER_SLEEP, (ULONG)unit, (ULONG)timeout, (ULONG)non_deferrable, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+}
+#endif
 
 qapi_Status_t qapi_Timer_Stop(qapi_TIMER_handle_t timer_handle) {
 	return (qapi_Status_t)_txm_module_system_call12(TXM_QAPI_TIMER_STOP, (ULONG)timer_handle, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
